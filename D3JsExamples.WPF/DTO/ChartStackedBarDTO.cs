8d2d62643c5bce0d5665d18ce66abf59f1efab2b
Ramon Valerio <rvalerio@tecgraf.puc-rio.br>
2017-02-10 11:39:18 -0200

    Stacked Bar

---
+++ D3JsExamples.WPF/DTO/ChartStackedBarDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace D3JsExamples.WPF.dto
+{
+    public class ChartStackedBarDTO
+    {
+        public string x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public int a { get; set; }
+        public static List<ChartStackedBarDTO> GetData()
+        {
+            var resultado = new List<ChartStackedBarDTO>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var dto = new ChartStackedBarDTO();
+                dto.x = string.Format("2011 Q{0}", i + 1);
+                dto.y = i + 1;
+                dto.z = i + 2;
+                dto.a = i + 3;
+
+                resultado.Add(dto);
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ D3JsExamples.WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using D3JsExamples.WPF.dto;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace D3JsExamples.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStackedBar_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowserChar.Source = new Uri(string.Format(@"file:///{0}/Html/stacked_bar/chart_stacked_bar.html", Directory.GetCurrentDirectory()));
+            //string json = JsonConvert.SerializeObject(ChartStackedBarDTO.GetData());
+
+            //webBrowserChar.InvokeScript("GerarGrafico");
+            //webBrowserChar.InvokeScript("GerarGrafico", "3", "1.123, 2.456, 3.789");
+            //webBrowserChar.InvokeScript("GerarGrafico", json);
+            //webBrowserChar.InvokeScript("ChartJS");
+        }
+    }
+}
